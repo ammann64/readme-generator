da424c809bdc76f4fefb1ab4f814fe511f9f386e
ammann64 <austinmmann64@gmail.com>
2023-10-07 03:04:13 -0500

    Fix license variable to be an object and add table of contents

--- index.js
+++ index.js
@@ -1,70 +1,81 @@
 const fs = require('fs');
-const inquirer = require('inquirer');
+const inquirer = require('./node_modules/inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 //List of available licenses in an array.
-const licenses = [{name: 'Apache License 2.0', value: 'apache'},
-{name: 'Artistic License 2.0', value: 'artistic'},
-{name: 'Boost Software License 1.0', value: 'bsl'},
-{name: 'Creative Commons Attribution 4.0', value: 'cc-by'},
-{name: 'Eclipse Public License 2.0', value: 'epl'},
-{name: 'GNU General Public License 3.0', value: 'gpl'},
-{name: 'Microsoft Public License', value: 'ms-pl'},
-{name: 'MIT', value: 'mit'},
-{name: 'Open Software License', value: 'osl'}]
+const licenses = [{name: 'Apache License 2.0', value: {license: 'Apache License 2.0', abbreviation: 'apache'}},
+{name: 'Artistic License 2.0', value: {license: 'Artistic License 2.0', abbreviation: 'artistic'}},
+{name: 'Boost Software License 1.0', value: {license: 'Boost Software License 1.0', abbreviation: 'bsl'}},
+{name: 'Creative Commons Attribution 4.0', value: {license: 'Creative Commons Attribution 4.0', abbreviation: 'cc-by'}},
+{name: 'Eclipse Public License 2.0', value: {license: 'Eclipse Public License 2.0', abbreviation: 'epl'}},
+{name: 'GNU General Public License 3.0', value: {license: 'GNU General Public License 3.0', abbreviation: 'gpl'}},
+{name: 'Microsoft Public License', value: {license: 'Microsoft Public License', abbreviation: 'ms-pl'}},
+{name: 'MIT', value: {license: 'MIT', abbreviation: 'mit'}},
+{name: 'Open Software License', value: {license: 'Open Software License', abbreviation: 'osl'}}]
 //Question objects to receive user input via inquirer
 const questions = [
     {
         type: 'input',
         message: 'What is your project title?',
-        name: 'title'
+        name: 'title',
+        default: ''
     },
     {
         type: 'input',
         message: 'What is your project description?',
-        name: 'description'
+        name: 'description',
+        default: ''
     },
     {
         type: 'input',
         message: 'How does the user install your application?',
-        name: 'installation'
+        name: 'installation',
+        default: ''
     },
     {
         type: 'input',
         message: 'How does the user use your application?',
-        name: 'usage'
+        name: 'usage',
+        default: ''
     },
     {
         type: 'input',
         message: 'What are the guidelines to contributing to your project?',
-        name: 'contribution'
+        name: 'contribution',
+        default: ''
     },
     {
         type: 'input',
         message: 'How does a developer test your project?',
-        name: 'testing'
+        name: 'testing',
+        default: ''
     },
     {
         type: 'list',
         message: 'Please choose a license:',
         name: 'license',
-        choices: licenses
+        choices: licenses,
+        default: {license: '', abbreviation: ''}
     },
     {
         type: 'input',
         message: 'Please enter your github username:',
-        name: 'username'
+        name: 'username',
+        default: ''
     },
     {
         type: 'input',
         message: 'Please enter your email address:',
-        name: 'email'
+        name: 'email',
+        default: ''
     }
 ];
 
 // Function to write README file
 function writeToFile(fileName, data) {
-   content = generateMarkdown.generateMarkdown(data);
-    fs.writeFile(fileName, content);
+   content = generateMarkdown(data);
+    fs.writeFile(fileName, content, (err) => {
+    err ? console.error(err) : console.log('Creating readme...') 
+    });
 }
 
 function init() {
@@ -74,7 +85,9 @@ function init() {
         
         .then((response) =>
         {
-            writeToFile('README.md', response); //Calls the write to file function to write the readme
+            // console.log(response);
+            // console.log(response.license);
+            writeToFile(`${response.title.trim()}README.md`, response); //Calls the write to file function to write the readme
         }
         );
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,66 +1,113 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// If there is no license, return an empty strings
+function renderLicenseBadge(license) {
+  let badge = '';
+  if (license != '') {
+    let abbreviation = license.abbreviation;
+    let link = renderLicenseLink(license.abbreviation);
+    badge = `${abbreviation} - ${link}`;
+  }
+  return badge;
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(abbreviation) {
   let link = '';
-  switch (license.value) {
-    case 'apache':
-      link = 'https://www.apache.org/licenses/LICENSE-2.0.html';
-      break;
-    case 'artistic':
-      link = 'https://www.perlfoundation.org/artistic-license-20.html';
-      break;
-    case 'bsl':
-      link = 'https://www.boost.org/users/license.html';
-      break;
-    case 'cc-by':
-      link = 'https://creativecommons.org/licenses/by/4.0/legalcode.en';
-      break;
-    case 'epl':
-      link = 'https://www.eclipse.org/legal/epl-2.0/';
-      break;
-    case 'gpl':
-      link = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
-      break;
-    case 'ms-pl':
-      link = 'https://learn.microsoft.com/en-us/previous-versions/msp-n-p/ff647676(v=pandp.10)?redirectedfrom=MSDN'
-      break;
-    case 'mit':
-      link = 'https://mit-license.org/';
-      break;
-    case 'osl':
-      link = 'https://opensource.org/license/osl-3-0-php/';
-      break;
+  if (abbreviation != '') {
+    switch (abbreviation) {
+      case 'apache':
+        link = 'https://www.apache.org/licenses/LICENSE-2.0.html';
+        break;
+      case 'artistic':
+        link = 'https://www.perlfoundation.org/artistic-license-20.html';
+        break;
+      case 'bsl':
+        link = 'https://www.boost.org/users/license.html';
+        break;
+      case 'cc-by':
+        link = 'https://creativecommons.org/licenses/by/4.0/legalcode.en';
+        break;
+      case 'epl':
+        link = 'https://www.eclipse.org/legal/epl-2.0/';
+        break;
+      case 'gpl':
+        link = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+        break;
+      case 'ms-pl':
+        link = 'https://learn.microsoft.com/en-us/previous-versions/msp-n-p/ff647676(v=pandp.10)?redirectedfrom=MSDN'
+        break;
+      case 'mit':
+        link = 'https://mit-license.org/';
+        break;
+      case 'osl':
+        link = 'https://opensource.org/license/osl-3-0-php/';
+        break;
+    }
   }
   return link;
 }
 
-// TODO: Create a function that returns the license section of README
+//Function to return license section of readme
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let section = '';
+  if (license != '') {
+    name = license.license;
+    link = renderLicenseLink(license.abbreviation);
+    section = `This project is licensed under ${name}. For more information please visit ${link}`;
+  }
+  return section;
+}
 
-// TODO: Create a function to generate markdown for README
+function renderTableOfContents() {
+  tableOfContents = 
+  `-[Description](#description)\n
+  -[Installation](#installation)\n
+  -[Usage](#usage)\n
+  -[Contribution](#contribution)\n
+  -[Testing](#testing)\n
+  -[License](#license)\n
+  -[Questions](#questions)
+  `
+  return tableOfContents;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  ## Description
-  ${description}
-  ## Installation
-  ${installation}
-  ## Usage
-  ${usage}
-  ## Contribution
-  ${contribution}
-  ## Testing
-  ${testing}
-  ## License
-  ${license.name}
-  ##Questions
-  Github Username: ${username}
-  Email: ${email}
-`;
+  const {title, description, installation, usage, contribution, testing, license, username, email} = data;
+  console.log(license);
+  let badge = renderLicenseBadge(license);
+  let licenseSection = renderLicenseSection(license);
+  let tableOfContents = renderTableOfContents();
+  content = `${badge}
+  # ${title}\n\n
+  ${tableOfContents}\n\n
+  <div id='description'/>\n
+  ## Description\n
+  
+  ${description}\n
+  <div id='installation'/>\n
+  ## Installation\n
+  ${installation}\n
+  <div id='usage'/>\n
+  ## Usage\n
+  ${usage}\n
+  <div id='contribution'/>\n
+  ## Contribution\n
+  ${contribution}\n
+  <div id='testing'/>\n
+  ## Testing\n
+  ${testing}\n
+  <div id='license'/>\n
+  ## License\n
+  ${licenseSection}\n
+  <div id='questions'>\n
+  ## Questions\n
+  Github Username: ${username}\n
+  Email: ${email}\n
+  `;
+  return content;
 }
 
 module.exports = generateMarkdown;
